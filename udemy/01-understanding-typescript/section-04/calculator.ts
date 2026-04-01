db24c2d3a865087165d152e28668f6a6e63c732f
starrykss <kss34891@gmail.com>
2026-04-01 20:35:19 +0900

    공부 내용 추가

---
+++ udemy/01-understanding-typescript/section-04/calculator.ts
@@ -0,0 +1,96 @@
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+type InvestmentData = {
+  initialAmount: number;
+  annualContribution: number;
+  expectedReturn: number;
+  duration: number;
+};
+
+type InvestmentResult = {
+  year: string;
+  totalAmount: number;
+  totalContributions: number;
+  totalInterestEarned: number;
+};
+
+type CalculationResult = InvestmentResult[] | string;
+
+// 계산
+function calculateInvestment(data: InvestmentData): CalculationResult {
+  const { initialAmount, annualContribution, expectedReturn, duration } = data;
+
+  if (initialAmount < 0) {
+    return 'Initial investment amount must be at least zero.';
+  }
+
+  if (duration <= 0) {
+    return 'No valid amount of years provided.';
+  }
+
+  if (expectedReturn <= 0) {
+    return 'Expected return must be at least zero.';
+  }
+
+  let total = initialAmount;
+  let totalContributions = 0;
+  let totalInterestEarned = 0;
+
+  const annualResults: InvestmentResult[] = [];
+
+  for (let i = 0; i < duration; i++) {
+    total = total * (1 + expectedReturn);
+    totalInterestEarned = total - totalContributions - initialAmount;
+    totalContributions = totalContributions + annualContribution;
+    total = total + annualContribution;
+
+    annualResults.push({
+      year: `Year ${i + 1}`,
+      totalAmount: total,
+      totalInterestEarned,
+      totalContributions,
+    });
+  }
+
+  return annualResults;
+}
+
+// 결과 출력
+function printResults(results: CalculationResult) {
+  // print (output) the result data
+  if (typeof results === 'string') {
+    console.log(results);
+    return;
+  }
+
+  for (const yearEndResult of results) {
+    console.log(yearEndResult.year);
+    console.log(`Total: ${yearEndResult.totalAmount.toFixed(0)}`);
+    console.log(
+      `Total Contributions: ${yearEndResult.totalContributions.toFixed(0)}`,
+    );
+    console.log(
+      `Total Interest Earned: ${yearEndResult.totalInterestEarned.toFixed(0)}`,
+    );
+    console.log('--------------------------------------');
+  }
+}
+
+//
+// main
+//
+
+const investmentData: InvestmentData = {
+  initialAmount: 5000,
+  annualContribution: 500,
+  expectedReturn: 0.08,
+  duration: 10,
+};
+
+const results = calculateInvestment(investmentData);
+
+printResults(results);
