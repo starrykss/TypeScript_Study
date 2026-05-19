400b4468a28a1bdd3a190ad98000c9c15b357c22
starrykss <kss34891@gmail.com>
2026-05-19 19:54:11 +0900

    공부 내용 추가

---
+++ udemy/01-understanding-typescript/section-16/webpack.config.prod.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  devtool: 'none',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // ? : 생략 가능
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+};
